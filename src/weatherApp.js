e11649fd4c3d0181742d5ff6543cc62b8a1398aa
goob0807 <122542069+goob8079@users.noreply.github.com>
2025-08-17 13:15:50 +0900

    webpack configuration

---
+++ src/weatherApp.js
@@ -0,0 +1,14 @@
+const locationInput = document.querySelector("#location-input").value;
+const submit = document.querySelector("#submit-btn");
+
+export async function weatherApp(location) {
+    try {
+        const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=V7NAKSYNRZ9LTZHF3ZNDUQXVD`);
+        const weatherData = await response.json();
+        console.log(weatherData);
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+submit.addEventListener("click", () => weatherApp(locationInput));
